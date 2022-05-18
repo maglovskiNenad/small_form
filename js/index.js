92ff5dfb319be63cac3fc33607c22f697b27e539
maglovskiNenad <maglovskin@gmail.com>
2022-05-18 09:44:17 +0200

    postavljena forma

---
+++ js/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const username_input = document.getElementById("username");
+const middlename_input = document.getElementById("middlename");
+const email_input = document.getElementById("email");
+const password_input = document.getElementById("password");
+const btn_login = document.getElementById("btn");
